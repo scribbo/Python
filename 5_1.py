51ed5bca7625fc960046546f4c208762c8573de5
Alexandra <scribbo@mail.ru>
2021-09-13 00:41:40 +0300

    5_1.py changed

--- 5_1.py
+++ 5_1.py
@@ -2,8 +2,7 @@ def gen(n):
     for i in range(1, n + 1):
         if i % 2 != 0:
             yield i
-        else:
-            continue
+
 
 gen_10 = gen(10)
 print(next(gen_10))
@@ -13,7 +12,3 @@ print(next(gen_10))
 print(next(gen_10))
 print(next(gen_10))
 print(next(gen_10))
-
-
-
-
