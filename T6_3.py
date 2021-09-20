638560e504e1b85ebb83809013b17fabaeab2ab0
Alexandra <scribbo@mail.ru>
2021-09-20 18:15:31 +0300

    HW_6

--- T6_3.py
+++ T6_3.py
@@ -5,7 +5,6 @@ with open('users.csv', 'r', encoding='utf-8') as f_users:
     name = f_users.read()
     names = name.split('\n')
 
-
 with open('hobby.csv', 'r', encoding='utf-8') as f_hobby:
     hobby = f_hobby.read()
     hobbies = hobby.split('\n')
--- T6_4.py
+++ T6_4.py
@@ -8,4 +8,3 @@ with open('users.csv', 'r', encoding='utf-8') as f_users:
         with open('my_dict.txt', 'w', encoding='utf-8') as f_dict:
             for el in us_hobby:
                 print(f"{str(el[0]).strip().replace(',', ' ')}: {str(el[1]).strip()}", file=f_dict)
-
